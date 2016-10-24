f3cfbd303f321d58e7e2a2bc210916db6337b732
LotusChing <986332598@qq.com>
2016-10-24 08:26:20 +0800

    Update client.py

--- practice/client.py
+++ practice/client.py
@@ -1,26 +1,14 @@
 # coding:utf8
-__author__ = 'LotusChing'
+import socket
+import readline
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(('localhost', 20000))
 
-from socketserver import BaseRequestHandler, TCPServer
-import subprocess
-
-class EchoHandler(BaseRequestHandler):
-    def handle(self):
-        print('Got connection from {}'.format(self.client_address))
-        while True:
-            cmd = self.request.recv(8192)
-            if not cmd:
-                self.request.send('Input is none.')
-            print(cmd)
-            data, err = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate()
-            print(data)
-            if err.decode() != '':
-                self.request.send(err)
-            if data.decode() == '':
-                self.request.send(b'OK.')
-            else:
-                self.request.send(data)
-
-if __name__ == '__main__':
-    serv = TCPServer(('', 20000), EchoHandler)
-    serv.serve_forever()
+while True:
+    inp = input('Please input: ').encode()
+    s.send(inp)
+    ret = s.recv(8192)
+    if ret == '':
+        continue
+    else:
+        print(ret.decode())
