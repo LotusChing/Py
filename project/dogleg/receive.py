52de7f5345c0882e3a6501827ce3fbbc93f0932b
root <root@node1.(none)>
2016-11-04 03:49:19 +0800

    Single server control Successful.

--- project/dogleg/receive.py
+++ project/dogleg/receive.py
@@ -1,24 +1,28 @@
 import pika
 import subprocess
 
-auth = pika.credentials.PlainCredentials(username='Da', password='123123')
+auth = pika.credentials.PlainCredentials(username='Lotus', password='Ching')
 connection = pika.BlockingConnection(pika.ConnectionParameters(
             host='120.24.80.34',
             port=80,
             credentials=auth))
 
 channel = connection.channel()
-channel.queue_declare(queue='hello')
+channel.queue_declare(queue='cmd')
 
 
 def callback(ch, method, properties, body):
+    print(body.decode())
     data, err = subprocess.Popen(body, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate()
-    print(data)
+    ch.basic_publish(exchange='',
+                     routing_key=properties.reply_to,
+                     body=str(data)
+                     )
+    ch.basic_ack(delivery_tag=method.delivery_tag)
+    print('Response commands result to Queue.')
 
-
-channel.basic_consume(callback,
-                      queue='hello',
-                      no_ack=True)
+channel.basic_qos(prefetch_count=1)
+channel.basic_consume(callback, queue='cmd')
 
 print(' [*] Waiting for messages. To exit press CTRL+C')
 channel.start_consuming()
--- project/dogleg/send.py
+++ project/dogleg/send.py
@@ -1,17 +1,42 @@
 # -*- coding:utf-8 -*-
-__author__ = "LotusChing"
 import pika
 
-auth = pika.credentials.PlainCredentials(username='Lotus', password='Ching')
-connection = pika.BlockingConnection(pika.ConnectionParameters(
-            host='120.24.80.34',
-            port=80,
-            credentials=auth))
 
-channel = connection.channel()
-channel.queue_declare(queue='hello')
-channel.basic_publish(exchange='',
-                      routing_key='hello',
-                      body='Hello World!')
-print(" [x] Sent 'Hello World!'")
-connection.close()
+class Center(object):
+    def __init__(self):
+        auth = pika.credentials.PlainCredentials(username='Lotus', password='Ching')
+        self.connection = pika.BlockingConnection(pika.ConnectionParameters(
+                    host='120.24.80.34',
+                    port=80,
+                    credentials=auth))
+        self.channel = self.connection.channel()
+
+        result = self.channel.queue_declare(queue='cmd')
+        self.callback_queue = result.method.queue
+
+        self.channel.basic_consume(self.on_response,
+                                   no_ack=True,
+                                   queue=self.callback_queue
+                                   )
+    def on_response(self, ch, method, props, body):
+        self.response = body
+
+    def request(self, n):
+        self.response = None
+        self.channel.basic_publish(exchange='',
+                                   routing_key='cmd',
+                                   properties=pika.BasicProperties(
+                                       reply_to=self.callback_queue
+                                        ),
+                                   body=str(n)
+                                   )
+        while self.response is None:
+            self.connection.process_data_events()
+        return self.response.decode()
+
+
+center = Center()
+while True:
+    commands = str(input('Please input commands: '))
+    response = center.request(commands)
+    print(response)
