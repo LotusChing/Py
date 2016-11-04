7bbe5f4aa3d7d03cfcbb614f6cc3ae96d565d6f6
LotusChing <986332598@qq.com>
2016-11-04 08:31:30 +0800

    with currelation id

--- project/dogleg/receive.py
+++ project/dogleg/receive.py
@@ -1,9 +1,9 @@
 import pika
 import subprocess
 
-auth = pika.credentials.PlainCredentials(username='Lotus', password='Ching')
+auth = pika.credentials.PlainCredentials(username='Da', password='Da')
 connection = pika.BlockingConnection(pika.ConnectionParameters(
-            host='120.24.80.34',
+            host='1.1.1.1',
             port=80,
             credentials=auth))
 
@@ -11,18 +11,23 @@ channel = connection.channel()
 channel.queue_declare(queue='cmd')
 
 
-def callback(ch, method, properties, body):
-    print(body.decode())
+def callback(ch, method, props, body):
     data, err = subprocess.Popen(body, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate()
     ch.basic_publish(exchange='',
-                     routing_key=properties.reply_to,
+                     routing_key=props.reply_to,
+                     properties=pika.BasicProperties(correlation_id=props.correlation_id),
                      body=str(data)
                      )
+
     ch.basic_ack(delivery_tag=method.delivery_tag)
     print('Response commands result to Queue.')
 
-channel.basic_qos(prefetch_count=1)
-channel.basic_consume(callback, queue='cmd')
-
-print(' [*] Waiting for messages. To exit press CTRL+C')
-channel.start_consuming()
+if __name__ == '__main__':
+    try:
+        channel.basic_qos(prefetch_count=1)
+        channel.basic_consume(callback, queue='cmd')
+        print(' [*] Waiting for messages. To exit press CTRL+C')
+        channel.start_consuming()
+    except KeyboardInterrupt:
+        print('Exit.\n')
+        exit()
--- project/dogleg/send.py
+++ project/dogleg/send.py
@@ -1,12 +1,13 @@
-# -*- coding:utf-8 -*-
 import pika
+import uuid
+deny_cmd = ['rm', 'dd', 'reboot', 'init', 'vim', 'service']
 
 
 class Center(object):
     def __init__(self):
-        auth = pika.credentials.PlainCredentials(username='Lotus', password='Ching')
+        auth = pika.credentials.PlainCredentials(username='Da', password='Da')
         self.connection = pika.BlockingConnection(pika.ConnectionParameters(
-                    host='120.24.80.34',
+                    host='1.1.1.1',
                     port=80,
                     credentials=auth))
         self.channel = self.connection.channel()
@@ -16,27 +17,33 @@ class Center(object):
 
         self.channel.basic_consume(self.on_response,
                                    no_ack=True,
-                                   queue=self.callback_queue
-                                   )
+                                   queue=self.callback_queue)
+
     def on_response(self, ch, method, props, body):
         self.response = body
 
     def request(self, n):
         self.response = None
+        self.corr_id = str(uuid.uuid4())
         self.channel.basic_publish(exchange='',
                                    routing_key='cmd',
                                    properties=pika.BasicProperties(
-                                       reply_to=self.callback_queue
+                                       reply_to=self.callback_queue,
+                                       correlation_id=self.corr_id
                                         ),
                                    body=str(n)
                                    )
         while self.response is None:
             self.connection.process_data_events()
-        return self.response.decode()
+        return self.response
 
-
-center = Center()
-while True:
-    commands = str(input('Please input commands: '))
-    response = center.request(commands)
-    print(response)
+if __name__ == '__main__':
+    try:
+        center = Center()
+        while True:
+            commands = str(input('Please input commands: '))
+            response = center.request(commands).decode()
+            print(response)
+    except KeyboardInterrupt:
+        print('Exit.\n')
+        exit()
