12b8133f66b5bd529a93db681e469fc8ff74c81b
刘聪 <congliu@wecan-info.com>
2017-03-18 00:20:31 +0800

    add log example

--- practice/pek_handler_log_parse.py
+++ practice/pek_handler_log_parse.py
@@ -7,6 +7,13 @@ import logging
 import requests
 import threading
 
+'''
+Log Example: 
+2017-03-07 10:09:29.029 [pool-2-thread-1] INFO  cn.aip.AsupTask[47] - 开始 待处理518条
+2017-03-07 10:17:04.004 [pool-2-thread-1] INFO  cn.aip.AsupTask[102] - 结束 解析518条，忽略0条,添加航班0条，更新航班518条，添加安检排队0条，添加出租车排队0
+2017-03-07 10:23:40.040 [pool-2-thread-1] WARN  cn.aip.service.ArrService[107] - 取消上一航班状态：632357|CA1292|延误|8929307
+'''
+
 event = threading.Event()
 logging.basicConfig(level=logging.INFO, format='%(asctime)s  %(levelname)s [%(threadName)s] - %(message)s')
 p = re.compile(r'(?P<time>\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}.\d{3}) \[(?P<thread>.*)\] (?P<level>\w{4}) (?P<class>.*) - (?P<remark>.*)')
